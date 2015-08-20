370e13e194e7f3cc0dee44d484267bb094808dda
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-20 09:20:41 +0300

    Add ball base code

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -17,6 +17,12 @@ Scene* HelloWorld::createScene()
     return scene;
 }
 
+HelloWorld::HelloWorld()
+	:m_physWorld(b2Vec2(0.0f, -9.8f))
+	,m_ballBody(nullptr)
+	,m_ballSprite(nullptr)
+{}
+
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
@@ -33,6 +39,62 @@ bool HelloWorld::init()
     /////////////////////////////
     // 3. add your codes below...
 
+	float sceneWidth = 480;
+	float sceneHeight = 640;
+
+	float ballStartX = sceneWidth / 2;
+	float ballStartY = sceneHeight - 100;
+	float ballRadius = 50;
+
+	float wallX = sceneWidth / 2;
+	float wallY = 75;
+	float wallWidth = 300;
+	float wallHeight = 50;
+
+	{
+		m_ballSprite = Sprite::create("ball.png");
+		addChild(m_ballSprite);
+
+		b2BodyDef ballBodyDef;
+		ballBodyDef.type = b2_dynamicBody;
+		ballBodyDef.position.Set(ballStartX / ptmRatio, ballStartY / ptmRatio);
+		ballBodyDef.userData = m_ballSprite;
+		m_ballBody = m_physWorld.CreateBody(&ballBodyDef);
+
+		b2CircleShape circle;
+		circle.m_radius = ballRadius / ptmRatio;
+
+		b2FixtureDef ballShapeDef;
+		ballShapeDef.shape = &circle;
+		ballShapeDef.density = 1.0f;
+		ballShapeDef.friction = 0.2f;
+		ballShapeDef.restitution = 0.8f;
+		m_ballBody->CreateFixture(&ballShapeDef);
+	}
+
+	{
+		auto wallSprite = Sprite::create("wall.png");
+		wallSprite->setPosition(ccp(wallX, wallY));
+		addChild(wallSprite);
+
+		b2BodyDef wallBodyDef;
+		wallBodyDef.type = b2_staticBody;
+		wallBodyDef.position.Set(wallX / ptmRatio, wallY / ptmRatio);
+		auto wallBody = m_physWorld.CreateBody(&wallBodyDef);
+
+		b2PolygonShape polygon;
+		polygon.SetAsBox(wallWidth / 2 / ptmRatio, wallHeight / 2 / ptmRatio);
+
+		b2FixtureDef wallShapeDef;
+		wallShapeDef.shape = &polygon;
+		wallShapeDef.density = 1.0f;
+		wallShapeDef.friction = 0.2f;
+		wallShapeDef.restitution = 0.8f;
+		wallBody->CreateFixture(&wallShapeDef);
+	}
+
+	//updateSprites();
+
     return true;
 }
 
@@ -45,3 +107,5 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
     exit(0);
 #endif
 }
+
+const float HelloWorld::ptmRatio = 32.0f;
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,12 +2,15 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "Box2D\Box2D.h"
 
 class HelloWorld : public cocos2d::Layer
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
+
+	HelloWorld();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
@@ -17,6 +20,13 @@ public:
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+private:
+	static const float ptmRatio;
+
+	b2World m_physWorld;
+	b2Body *m_ballBody;
+	cocos2d::Sprite *m_ballSprite;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
