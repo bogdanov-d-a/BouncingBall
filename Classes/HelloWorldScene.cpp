e9323ca34277b71f6326841264d7eb5f23489dd0
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-21 09:06:54 +0300

    Scene made failsafe

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -9,9 +9,13 @@ Scene* HelloWorld::createScene()
 {
 	// 'scene' is an autorelease object
 	auto scene = Scene::create();
+	if (!scene)
+		return nullptr;
 	
 	// 'layer' is an autorelease object
 	auto layer = HelloWorld::create();
+	if (!layer)
+		return nullptr;
 
 	// add layer as a child to scene
 	scene->addChild(layer);
@@ -21,26 +25,19 @@ Scene* HelloWorld::createScene()
 }
 
 HelloWorld::HelloWorld()
-	:m_physEngine(nullptr)
-	,m_ball(nullptr)
-	,m_wall(nullptr)
+	: m_physEngine(nullptr)
+	, m_ball(nullptr)
+	, m_wall(nullptr)
 {}
 
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-	//////////////////////////////
-	// 1. super init first
-	if ( !Layer::init() )
-	{
+	if (!Layer::init())
 		return false;
-	}
-	
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	/////////////////////////////
-	// 3. add your codes below...
+	//Size visibleSize = Director::getInstance()->getVisibleSize();
+	//Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 	float ballStartX = 240;
 	float ballStartY = 540;
@@ -52,12 +49,18 @@ bool HelloWorld::init()
 	float wallHeight = 50;
 
 	m_physEngine = PhysicsEngine::create(this, 32);
+	if (!m_physEngine)
+		return false;
 	addChild(m_physEngine);
 
 	m_ball = Ball::create(m_physEngine, b2Vec2(ballStartX / m_physEngine->getPtmRatio(), ballStartY / m_physEngine->getPtmRatio()), ballRadius / m_physEngine->getPtmRatio(), this);
+	if (!m_ball)
+		return false;
 	this->addChild(m_ball);
 
 	m_wall = Wall::create(m_physEngine, b2Vec2(wallX / m_physEngine->getPtmRatio(), wallY / m_physEngine->getPtmRatio()), Size(wallWidth / m_physEngine->getPtmRatio(), wallHeight / m_physEngine->getPtmRatio()));
+	if (!m_wall)
+		return false;
 	this->addChild(m_wall);
 
 	return true;
